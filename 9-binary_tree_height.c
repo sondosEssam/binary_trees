77157bab20600d22a1c5beab05e192be90117bb1
SondosEssam <sondosessam713@gmail.com>
2024-06-07 15:41:26 +0300

    plsword

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int b;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
 	n = binary_tree_height(tree->left);
 	b = binary_tree_height(tree->right);
 	return ((n >= b ? n + 1 : b + 1));
