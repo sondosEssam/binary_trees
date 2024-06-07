fe2800773758f63d664a52b4e8e9b3fbc2e5962a
SondosEssam <sondosessam713@gmail.com>
2024-06-07 18:41:38 +0300

    sibligns

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_sibling - function
+ * @node: parent
+ * Return: none
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->right)
+		return (node->parent->left);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (NULL);
+}
