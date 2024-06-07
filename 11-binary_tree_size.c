716bb4ec76d71de4e3ee3d8618c1a20f3ccf74eb
SondosEssam <sondosessam713@gmail.com>
2024-06-07 15:30:09 +0300

    declartion

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,10 +6,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t n;
+	size_t b;
+
 	if (tree == NULL)
 		return (0);
-	int n = binary_tree_size(tree->left);
-	int b = binary_tree_size(tree->right);
+	 n = binary_tree_size(tree->left);
+	 b = binary_tree_size(tree->right);
 
 	return (n + b + 1);
 }
