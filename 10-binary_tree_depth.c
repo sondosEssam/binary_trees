e512215027a891b981aa5508eea23104931bd36c
SondosEssam <sondosessam713@gmail.com>
2024-06-07 14:53:46 +0300

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_depth - function
+ * @tree: parent
+ * Return: none
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree->parent == NULL)
+		return (0);
+	int n = binary_tree_depth(tree->parent);
+
+	return (n + 1);
+}
